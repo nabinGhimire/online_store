1b6b571c96d043f269e6143f6b9b9ef384d8ecb0
Nabin Ghimire <nabin@hamro.com>
2024-02-21 11:52:44 +0545

    Registration form updated with countries

--- register.php
+++ register.php
@@ -1,26 +1,20 @@
 <?php 
+
+echo "<pre>";
+print_r($_GET);
+echo "</pre>";
+
     $title = "Register Page";
     $active = "register";
     require('includes/header.php'); 
 
-    $country = [
-        "nepal" => "Nepal",
-        "india" => "India"
-    ];
+    $countries = ["Nepal","India","China","Russia","Japan"];
 ?>
 
-<h1>Register New Account</h1>
-    <form>
+    <h1 style="text-align:center;">Register New Account</h1>
+    <form style="width: 500px; background-color: #ccc; padding: 10px; margin: auto;">
         Name : 
-        <input type="text" name="name" id="name">
-        <span class="error">
-            <?php 
-            if(isset($name_error)){
-                echo $name_error;  
-            }
-            ?>
-        </span>
-        <br>
+        <input type="text" name="name" id="name"><br>
         Email: <input type="text" name="email" id="email"><br>
         Phone: <input type="text" name="phone" id="phone"><br>
         Gender: <br>
@@ -30,14 +24,16 @@
         <select name="country" id="country">
             <option value="">Select a Country</option>
             <?php
-                foreach($country as $key => $value){
+                foreach($countries as $key => $value){
                     echo "<option value='$key'>$value</option>";
                 }
             ?>
-
+            <!-- <option value="nepal">Nepal</option>
+            <option value="india">India</option> -->
         </select><br>
-        Date of Birth: <input type="date" name="dob" id="dob">
-
+        Date of Birth: <input type="date" name="dob" id="dob"><br>
+        Password: <input type="password" name="password" id="password"><br>
+        Confirm Password: <input type="password" name="confirm_password" id="confirm_password">
         <br>
         <input type="submit" name="register" value="Register">
     </form>
